e19fc2bfe4e1489d87e459458a7e64f179365fe8
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2013-01-22 21:06:56 +0100

    Kill dead routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 BostonRuby::Application.routes.draw do
-  %w{jobs projects users companies}.each do |dead_route|
-    match "/#{dead_route}" => redirect('/')
-  end
-
   root :to => 'high_voltage/pages#show', :id => 'home'
   get '/calendar' => 'high_voltage/pages#show', :id => 'calendar', :as => 'calendar'
   get '/presentations/month/:month/' => 'presentations#index', :as => 'month_presentations'
