cd1c4f2ff60f957eb7d66a8d85dcb552b22ca43a
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2013-02-17 20:31:44 +0100

    Don't store secret token in version control

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BostonRuby::Application.config.secret_token = '74e74f6d2f13b377b609f17a20d4d397dbf0d3820e53260fffb940a1fab8fe241c652cfdc9642abed4a64e4fc2f30cadd5730cd926244bd7de28dcf51a0775fe'
+BostonRuby::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'fallback_token_for_development'
